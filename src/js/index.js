6642c72b62558d9bf3634eb9c87cc4995d723c5a
Prasad19122002 <147840443+Prasad19122002@users.noreply.github.com>
2024-08-03 07:00:20 +0530

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,34 @@
+// Get slide and canvas elements
+let firstSlideContainer = document.getElementsByClassName("slide--content")[0];
+let secondSlideContainer = document.getElementsByClassName("slide--content--one")[0];
+let secondCanvas = document.getElementsByClassName("second--canvas")[0];
+let thirdCanvas = document.getElementsByClassName("third--canvas")[0];
 
-let firstSlideContainer = 
-document.getElementsByClassName("slide--content")[0];
+// Create and configure the audio element
+let audio = document.getElementById('background-audio');
+audio.src = 'src/images/s1.mp3'; // Specify the path to your audio file
+audio.loop = true; // Ensure the audio loops
+audio.autoplay = true; // Start playing the audio automatically
 
-let secondSlideContainer = 
-document.getElementsByClassName("slide--content--one")[0];
+// Initially hide second slide and canvases
+secondSlideContainer.setAttribute("style", "display:none");
+secondCanvas.setAttribute("style", "display:none");
+thirdCanvas.setAttribute("style", "display:none");
 
-let secondCanvas = 
-document.getElementsByClassName("second--canvas")[0];
+// Show second slide after 2.5 seconds
+let containerToggleOne = setTimeout(function() {
+  firstSlideContainer.setAttribute("style", "display:none");
+  secondSlideContainer.setAttribute("style", "display:block");
+}, 2500);
 
-secondSlideContainer.setAttribute("style","display:none");
-secondCanvas.setAttribute("style","display:none");
+// Remove first slide and show second canvas after 6.5 seconds
+let removeFirstSlide = setTimeout(function() {
+  document.getElementsByClassName("first--slide")[0].setAttribute("style", "display:none;");
+  secondCanvas.setAttribute("style", "display:block");
+}, 6500);
 
-let thirdCanvas = 
-document.getElementsByClassName("third--canvas")[0];
-
-thirdCanvas.setAttribute("style","display:none");
-
-
-
-let containerToggleOne = setTimeout(function(){
-	firstSlideContainer.setAttribute("style","display:none");
-	secondSlideContainer.setAttribute("style","display:block");
-},2500);
-
-
-let removeFirstSlide = setTimeout(function(){
-	document.getElementsByClassName("first--slide")[0].setAttribute("style","display:none;")
-	secondCanvas.setAttribute("style","display:block");
-},6500);
-
-let removeSecondCanvas = setTimeout(function(){
-	secondCanvas.setAttribute("style","display:none");
-	thirdCanvas.setAttribute("style","display:block")
-},9800)
+// Remove second canvas and show third canvas after 9.8 seconds
+let removeSecondCanvas = setTimeout(function() {
+  secondCanvas.setAttribute("style", "display:none");
+  thirdCanvas.setAttribute("style", "display:block");
+}, 9800);
